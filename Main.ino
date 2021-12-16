2246fe93ad07fd04b7feb62aa2c10da61140800e
Methodician <methodician@gmail.com>
2021-12-16 08:37:15 -0800

    Basically working program!

--- Main.ino
+++ Main.ino
@@ -24,7 +24,7 @@ Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-int     mode     = 0;    // Currently-active mode, 0-9
+boolean isOn = true;
 boolean oldColorState      = HIGH;
 boolean oldBrightnessState = HIGH;
 boolean oldComboState [2] = { HIGH, HIGH };
@@ -38,7 +38,7 @@ void setup() {
   pinMode(BRIGHTNESS_BTN_PIN, INPUT_PULLUP);
   
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  setHue();            // Turn OFF all pixels ASAP
+  setHue();
 }
 
 void loop() {
@@ -48,24 +48,27 @@ void loop() {
 }
 
 void watchComboPress () {
-  boolean newComboState [2] = { digitalRead(BRIGHTNESS_BTN_PIN), digitalRead(COLOR_BTN_PIN)};
+  boolean newComboState [2] = { digitalRead(BRIGHTNESS_BTN_PIN), digitalRead(COLOR_BTN_PIN) };
 
-  if((newComboState == { LOW, LOW }) && (oldComboState == { HIGH, HIGH })) {
-    delay(20);
+  if((newComboState[0] == LOW) && (newComboState[1] == LOW) && (oldComboState[0] == HIGH) && (oldComboState[1] == HIGH )) {
+    
+    delay(60);
 
-    newComboState = { digitalRead(BRIGHTNESS_BTN_PIN), digitalRead(COLOR_BTN_PIN)};
+    newComboState[0] = digitalRead(BRIGHTNESS_BTN_PIN);
+    newComboState[1] = digitalRead(COLOR_BTN_PIN);
 
-    if(newComboState == { LOW, LOW}) {
-      if(strip.getBrightness() == 0) {
-        strip.setBrightness(25);
+    if(newComboState[0] == LOW && newComboState[1] == LOW) {
+      if(isOn) {
+        isOn = false;
       } else {
-        strip.setBrightness(0);
+        isOn = true;
       }
-        strip.show();
+        setHue();
     }
 
-  oldComboState = newComboState;
   }
+  oldComboState[0] = newComboState[0];
+  oldComboState[1] = newComboState[1];
 }
 
 void watchBrightnessButton () {
@@ -75,10 +78,10 @@ void watchBrightnessButton () {
   if((newBrightnessState == LOW) && (oldBrightnessState == HIGH)) {
     // Short delay to debounce button.
     delay(20);
-    // Check if button is still low after debounce.8
+    // Check if button is still low after debounce.
     newBrightnessState = digitalRead(BRIGHTNESS_BTN_PIN);
     if (newBrightnessState == LOW) {      // Yes, still low
-      if (brightness < 255) {
+      if (brightness < 254) {
         brightness += 1;
       } else {
         brightness = 25;
@@ -87,11 +90,6 @@ void watchBrightnessButton () {
       setHue();
     }
   }
-}
-
-void setBrightness() {
-  strip.setBrightness(brightness); 
-  strip.show();
 }
 
 void watchColorButton () {
@@ -117,47 +115,14 @@ void watchColorButton () {
 }
 
 void setHue () {
+  if(!isOn) {
+    brightness = 0;
+  } else {
+    if(brightness == 0) {
+      brightness = 25;
+      hue = 0;
+    }
+  }
    strip.rainbow(hue, 1, 255, brightness, true);
    strip.show();
 }
-
-void setColor(uint32_t color) {
-  strip.setPixelColor(0, color);
-  strip.show();
-}
-
-void setColorAndBrightness(uint32_t color, int brightness){
-  strip.setPixelColor(0, color);
-  strip.setBrightness(brightness); //(max = 255)
-  strip.show();
-}
-
-// The below seem to block use of the button...
-
-// void colorCycle(int wait, uint8_t brightness) {
-//  for(long color = 0; color < 5*65536; color += 256) {
-//    strip.rainbow(color, 9999, 255, brightness, true);
-//    strip.show();
-//    delay(wait);
-//  }  
-// }
-
-//// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-//void rainbow(int wait) {
-//  // Hue of first pixel runs 5 complete loops through the color wheel.
-//  // Color wheel has a range of 65536 but it's OK if we roll over, so
-//  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-//  // means we'll make 5*65536/256 = 1280 passes through this loop:
-//  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-//    // strip.rainbow() can take a single argument (first pixel hue) or
-//    // optionally a few extras: number of rainbow repetitions (default 1),
-//    // saturation and value (brightness) (both 0-255, similar to the
-//    // ColorHSV() function, default 255), and a true/false flag for whether
-//    // to apply gamma correction to provide 'truer' colors (default true).
-//    strip.rainbow(firstPixelHue);
-//    // Above line is equivalent to:
-//    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-//    strip.show(); // Update strip with new contents
-//    delay(wait);  // Pause for a moment
-//  }
-//}
